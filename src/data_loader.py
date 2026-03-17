3405ca283c954e927d25ea12bc8ea716855322fa
aaritdua <aaritduaofficial@gmail.com>
2026-03-16 21:10:14 -0400

    add load_passing_data with caching

---
+++ src/data_loader.py
@@ -0,0 +1,26 @@
+from nba_api.stats.endpoints import PlayerDashPtPass, CommonTeamRoster
+from nba_api.stats.static import teams, players
+import pandas as pd
+import time
+
+def load_passing_data(team_id: int, season: str) -> pd.DataFrame:
+    """
+    Load passing data for all players on a given NBA team for a given season.
+    
+    Return a DataFrame where each row represents a passing relationship between two players
+    """
+    try:
+        result = pd.read_csv(f"../data/passing_{team_id}_{season}.csv")
+    except:
+        roster = CommonTeamRoster(team_id, season).get_data_frames()[0]
+        all_data = []
+        for player in roster["PLAYER_ID"]:
+            df = PlayerDashPtPass(player_id=player, team_id=team_id, season=season).get_data_frames()[0]
+            all_data.append(df)
+            time.sleep(0.6)
+        result = pd.concat(all_data, ignore_index = True)
+        result.to_csv(f"../data/passing_{team_id}_{season}.csv")
+    return result
+
+
+# def load_play_by_play(game_id: str) -> pd.DataFrame:
