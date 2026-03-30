6403e3c3a95f714640a01ca188208e9f191062bf
LucHue <142170095+LucHue@users.noreply.github.com>
2026-03-30 03:15:30 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+"""CSC111 Winter 2026 Project 2: Mapping the Flow
+
+Created by: Aarit Dua, Vedant Kansara, Lucas Hui
+
+Title: main
+Description: This file runs the project
+
+Copyright and Usage Information
+===============================
+
+This file is provided for educational and personal use only. You may view, download, and modify the code for your own
+non-commercial purposes, provided that proper credit is given to the original author.
+You may not redistribute, publish, or use this project or any modified version of it for commercial purposes without
+explicit written permission from the author.
+This project may include third-party libraries, data, or tools that are subject to their own licenses and terms of use.
+Users are responsible for reviewing and complying with those licenses.
+"""
+from visualization import run_visualization
+
+run_visualization()
